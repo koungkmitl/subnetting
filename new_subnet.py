391c345955ac73639c8522c6deec48deb3972777
sirbank <isirbank@gmail.com>
2015-05-13 23:29:16 +0700

    Add function check overlap and implement it

--- new_subnet.py
+++ new_subnet.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from netaddr import *
+from bisect import *
 import pprint
 import sys
 
@@ -8,7 +9,7 @@ networkaddr = []
 
 def usedMask(number):
     for i in xrange(16):
-        if 2**i >= number+2:
+        if 2**i >= number+2: #include network address and broadcast address
             cider = 2**i
             break
         else:
@@ -16,21 +17,53 @@ def usedMask(number):
     # cidr = host
     return 32-i
 
-def firstSubnet(ip, number):
+def firstSubnet(number):
     cidr = usedMask(number)
-    temp_ip = str(ip.ip) + "/" + str(cidr)
-    ip = IPNetwork(temp_ip)
-    networkaddr.append(ip)
-    print networkaddr
+    subnets = list(ip.subnet(cidr))
+    subnets.sort()
+    possibleList = []
+    if len(networkaddr):
+        for i in xrange(len(subnets)):
+            net = [networkaddr[-1], subnets[i]] # check overlap between last used subnet and possible subnet
+            # print 'Compare [Last used, Possible]:', net
+            if not(subnets_overlap(net)) and (networkaddr[-1] < subnets[i]): # if not overlap and current subnet less than possible subnet
+                possibleList.append(subnets[i]) # add possible subnet to list
+                # print 'Add possible subnet to list:', subnets[i]
+        # print '============= Show possible subnet ============='
+        # print possibleList
+        # print '================================================'
+        networkaddr.append(possibleList[0]) # put lowest possible subnet to subnet table
+        # print 'Add possible subnet to subnet table:', possibleList[0]
+    else:
+        networkaddr.append(subnets[0]) # put first subnet to subnet table
+
+def subnets_overlap(subnets):
+    # ranges will be a sorted list of alternating start and end addresses
+    ranges = []
+    for subnet in subnets:
+        # find indices to insert start and end addresses
+        first = bisect_left(ranges, subnet.first)
+        last = bisect_right(ranges, subnet.last)
+        # check the overlap conditions and return if one is met
+        if first != last or first % 2 == 1:
+            return True
+        ranges[first:first] = [subnet.first, subnet.last]
+    return False
 
 def main():
+    global ip
     amount = []
     ip = IPNetwork(raw_input("Enter IP Address with subnet mask: "))
     subnet_amount = input("Enter amount of subnet: ")
     for i in xrange(subnet_amount):
         amount.append(input("Enter amount of hosts: "))
     for i in xrange(subnet_amount):
-        firstSubnet(ip, amount[i])
+        firstSubnet(amount[i])
+    # show subnet table
+    for result in networkaddr:
+        # print '========== Subnet table =========='
+        print result, result.netmask
+        # print '=================================='
 
 if __name__ == "__main__":
     main()

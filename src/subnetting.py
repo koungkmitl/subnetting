877a9498fe8a276c79c0cd00655452121beaee23
sirbank <isirbank@gmail.com>
2015-05-14 01:47:32 +0700

    Rename firstSubnet to calculateSubnet and Modify result ui

--- src/subnetting.py
+++ src/subnetting.py
@@ -2,39 +2,28 @@
 # -*- coding: utf-8 -*-
 
 from netaddr import *
-from bisect import *
+from bisect import bisect_left, bisect_right
 import pprint
 import sys
 
-networkaddr = []
-
 def usedMask(number):
-    for i in xrange(16):
-        if 2**i >= number+2: #include network address and broadcast address
-            cider = 2**i
-            break
-        else:
-            2**i
-    # cidr = host
+    i = 1
+    while(2**i < number+2):
+        i = i +1
     return 32-i
 
-def firstSubnet(number):
+def calculateSubnet(number):
     cidr = usedMask(number)
     subnets = list(ip.subnet(cidr))
     subnets.sort()
-    possibleList = []
     if len(networkaddr):
         for i in xrange(len(subnets)):
             net = [networkaddr[-1], subnets[i]] # check overlap between last used subnet and possible subnet
             # print 'Compare [Last used, Possible]:', net
             if not(subnets_overlap(net)) and (networkaddr[-1] < subnets[i]): # if not overlap and current subnet less than possible subnet
-                possibleList.append(subnets[i]) # add possible subnet to list
-                # print 'Add possible subnet to list:', subnets[i]
-        # print '============= Show possible subnet ============='
-        # print possibleList
-        # print '================================================'
-        networkaddr.append(possibleList[0]) # put lowest possible subnet to subnet table
-        # print 'Add possible subnet to subnet table:', possibleList[0]
+                # print 'Add to subnet table:', subnets[i]
+                networkaddr.append(subnets[i]) # put  possible subnet to subnet table
+                break
     else:
         networkaddr.append(subnets[0]) # put first subnet to subnet table
 
@@ -53,17 +42,20 @@ def subnets_overlap(subnets):
 
 def main():
     global ip
+    global networkaddr
+    networkaddr = []
     amount = []
     ip = IPNetwork(raw_input("Enter IP Address with subnet mask: "))
     subnet_amount = input("Enter amount of subnet: ")
     for i in xrange(subnet_amount):
         amount.append(input("Enter amount of hosts: "))
     for i in xrange(subnet_amount):
-        firstSubnet(amount[i])
+        calculateSubnet(amount[i])
     # show subnet table
+    print 'Network address\t\tBroadcast address\tSubnet mask\t\tCIDR'
     for result in networkaddr:
         # print '========== Subnet table =========='
-        print result, result.netmask
+        print '%s\t\t%s\t\t%s\t\t%s' % (result.network, result.broadcast, result.netmask, result.prefixlen)
         # print '=================================='
 
 if __name__ == "__main__":
